85a82fc74051bf946bc90102e20b4bdd8e0a3508
mrcn21 <145170102+mrcn21@users.noreply.github.com>
2023-11-03 00:00:40 +0800

    Add files via upload

---
+++ Lorenzonian Portal.php
@@ -0,0 +1,39 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Form</title>
+</head>
+<body>
+
+    <center>
+
+
+        <div>
+
+            <form>
+
+                <div>
+                    <label>Username</label>
+                    <input type="text" name="username">
+                </div>
+
+                <div>
+                    <label>Password</label>
+                    <input type="Password" name="password">
+                </div>
+
+                <div>
+                    
+                    <input type="submit" name="submit" value="Login">
+                </div>
+
+            </form>
+
+
+        </div>
+
+    </center>
+
+</body>
+</html>
